b76a32bbb04a51f8b02970b95c6c3c8db354a9cc
nick <nizyt1@163.com>
2024-09-25 22:42:58 +0800

    创建国际化、本地化资源
    创建国际化、本地化资源

--- src/main/java/com/chunqiuxu/bronzeage/content/blocks/BlockRegistry.java
+++ src/main/java/com/chunqiuxu/bronzeage/content/blocks/BlockRegistry.java
@@ -13,10 +13,6 @@ public class BlockRegistry{
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block",
-            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-
     public static final RegistryObject<Block> TIN_ORE_BLOCK = BLOCKS.register("tin_ore_block",
             () -> new TinOreBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK)));
 
